f5bdba6223fb2ba2c6f644231f90a811a6d07d85
Al Rafi <alrafi4@gmail.com>
2024-11-15 20:26:00 +0600

    store it setupandmetadata,eslintandprettier,authlayout,authform,otpmodal,appwrite,layoutnavsidebar

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="text-3xl text-brand-100">Store It - The only storage solution you need.</h1>
+    </div>
+  );
+}
