a48718f88a51d9247acadfb598f55b1d716c9ee5
Al Rafi <alrafi4@gmail.com>
2024-11-19 15:03:53 +0600

    dynamic file routes

--- lib/actions/file.action.ts
+++ lib/actions/file.action.ts
@@ -62,12 +62,7 @@ export const uploadFile = async ({
 };
 
 const createQueries = (
-  currentUser: Models.Document,
-  types: string[],
-  searchText: string,
-  sort: string,
-  limit?: number,
-) => {
+  currentUser: Models.Document) => {
   const queries = [
     Query.or([
       Query.equal("owner", [currentUser.$id]),
@@ -75,27 +70,22 @@ const createQueries = (
     ]),
   ];
 
-  if (types.length > 0) queries.push(Query.equal("type", types));
-  if (searchText) queries.push(Query.contains("name", searchText));
-  if (limit) queries.push(Query.limit(limit));
+  // if (types.length > 0) queries.push(Query.equal("type", types));
+  // if (searchText) queries.push(Query.contains("name", searchText));
+  // if (limit) queries.push(Query.limit(limit));
 
-  if (sort) {
-    const [sortBy, orderBy] = sort.split("-");
+  // if (sort) {
+  //   const [sortBy, orderBy] = sort.split("-");
 
-    queries.push(
-      orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy),
-    );
-  }
+  //   queries.push(
+  //     orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy),
+  //   );
+  // }
 
   return queries;
 };
 
-export const getFiles = async ({
-  types = [],
-  searchText = "",
-  sort = "$createdAt-desc",
-  limit,
-}: GetFilesProps) => {
+export const getFiles = async () => {
   const { databases } = await createAdminClient();
 
   try {
@@ -103,7 +93,7 @@ export const getFiles = async ({
 
     if (!currentUser) throw new Error("User not found");
 
-    const queries = createQueries(currentUser, types, searchText, sort, limit);
+    const queries = createQueries(currentUser);
 
     const files = await databases.listDocuments(
       appwriteConfig.databaseId,
